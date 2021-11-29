c1364db83251adace4d8673b899ae62d76ccff58
Your Name <mohamedfarah@Mohameds-MacBook-Pro.local>
2021-11-29 11:05:36 -0800

    preparing to deploy

--- js/main.js
+++ js/main.js
@@ -1,43 +1,27 @@
 /*----- constants -----*/
-const seven = document.createElement("img")
-seven.src = "imgs/seven.png" 
-const cherry = document.createElement("img")
-cherry.src = "imgs/cherry.png"
-const heart = document.createElement("img")
-heart.src = "imgs/heart.png" 
-
-const seven2 = document.createElement("img")
-seven2.src = "imgs/seven.png" 
-const cherry2 = document.createElement("img")
-cherry2.src = "imgs/cherry.png"
-const heart2 = document.createElement("img")
-heart2.src = "imgs/heart.png" 
-
-const seven3 = document.createElement("img")
-seven3.src = "imgs/seven.png" 
-const cherry3 = document.createElement("img")
-cherry3.src = "imgs/cherry.png"
-const heart3 = document.createElement("img")
-heart3.src = "imgs/heart.png" 
-
+let seven = 'imgs/seven.png'
+let cherry = 'imgs/cherry.png'
+let heart = 'imgs/heart.png'
+let wallet = 3000
 const winningCombos = [
     [0, 0, 0],
     [1, 1, 1],
     [2, 2, 2]
 ]
 /*----- app's state (variables) -----*/
-let board; 
+let board;
 
 /*----- cached element references -----*/
+let walletEl = document.getElementById('wallet')
 const resultEl = document.querySelector('h2');
 const buttonEl = document.getElementById('btn')
 const buttonEl2 = document.getElementById('btn2')
 const slotOne = document.getElementById('slot1')
 const slotTwo = document.getElementById('slot2')
 const slotThree = document.getElementById('slot3')
-const imgOne = document.getElementById('one')
-const imgTwo = document.getElementById('two')
-const imgThree = document.getElementById('three')
+const imgOne = document.createElement('img')
+const imgTwo = document.createElement('img')
+const imgThree = document.createElement('img')
 /*----- event listeners -----*/
 buttonEl.addEventListener('click', pullLever)
 buttonEl2.addEventListener('click', init)
@@ -46,9 +30,11 @@ buttonEl2.addEventListener('click', init)
 init()
 function init() {
     board = [null, null, null]
-    slotOne.replaceChildren(imgOne)
-    slotTwo.replaceChildren(imgTwo)
-    slotThree.replaceChildren(imgThree)
+    wallet = 3000
+    slotOne.replaceChildren()
+    slotTwo.replaceChildren()
+    slotThree.replaceChildren()
+    resultEl.innerText = ''
 }
 function pullLever() {
     board = [
@@ -56,47 +42,58 @@ function pullLever() {
         Math.floor(Math.random() * 3),
         Math.floor(Math.random() * 3)
     ];
+    wallet = wallet - 250
+    walletEl
     renderSlotOne()
     renderSlotTwo()
     renderSlotThree()
     checkWin()
-} 
+}
 function checkWin() {
     if (everyHelper(board, winningCombos[0])) {
-           renderJackpotMessage()
-       } else if (everyHelper(board, winningCombos[1])) {
-           renderWinningMessage()
-       } else if (everyHelper(board, winningCombos[2])) {
-           renderWinningMessage()
-       } else {
-           renderLosingMessage()
-       }
-} 
+        renderJackpotMessage()
+    } else if (everyHelper(board, winningCombos[1])) {
+        renderWinningMessage()
+    } else if (everyHelper(board, winningCombos[2])) {
+        renderWinningMessage()
+    } else {
+        renderLosingMessage()
+    }
+}
 function renderSlotOne() {
     if (board[0] === 0) {
-        imgOne.replaceWith(seven)
+        imgOne.src = seven
+        slotOne.append(imgOne)
     } else if (board[0] === 1) {
-        imgOne.replaceWith(cherry)
+        imgOne.src = cherry
+        slotOne.append(imgOne)
     } else if (board[0] === 2) {
-        imgOne.replaceWith(heart)
-    } 
-} 
+        imgOne.src = heart
+        slotOne.append(imgOne)
+    }
+}
 function renderSlotTwo() {
     if (board[1] === 0) {
-        imgTwo.replaceWith(seven2)
+        imgTwo.src = seven
+        slotTwo.append(imgTwo)
     } else if (board[1] === 1) {
-        imgTwo.replaceWith(cherry2) 
+        imgTwo.src = cherry
+        slotTwo.append(imgTwo)
     } else if (board[1] === 2) {
-        imgTwo.replaceWith(heart2)
+        imgTwo.src = heart
+        slotTwo.append(imgTwo)
     }
 }
 function renderSlotThree() {
     if (board[2] === 0) {
-        imgThree.replaceWith(seven3)
+        imgThree.src = seven
+        slotThree.append(imgThree)
     } else if (board[2] === 1) {
-        imgThree.replaceWith(cherry3)
+        imgThree.src = cherry
+        slotThree.append(imgThree)
     } else if (board[2] === 2) {
-        imgThree.replaceWith(heart3)
+        imgThree.src = heart
+        slotThree.append(imgThree)
     }
 }
 //Result Prompts
