a1d44cd4e7976fb0c1d12e84ef72c024926048e4
Your Name <mohamedfarah@Mohameds-MacBook-Pro.local>
2021-11-28 19:09:19 -0800

    fixed reset button and issues with images

--- js/main.js
+++ js/main.js
@@ -6,6 +6,20 @@ cherry.src = "imgs/cherry.png"
 const heart = document.createElement("img")
 heart.src = "imgs/heart.png" 
 
+const seven2 = document.createElement("img")
+seven2.src = "imgs/seven.png" 
+const cherry2 = document.createElement("img")
+cherry2.src = "imgs/cherry.png"
+const heart2 = document.createElement("img")
+heart2.src = "imgs/heart.png" 
+
+const seven3 = document.createElement("img")
+seven3.src = "imgs/seven.png" 
+const cherry3 = document.createElement("img")
+cherry3.src = "imgs/cherry.png"
+const heart3 = document.createElement("img")
+heart3.src = "imgs/heart.png" 
+
 const winningCombos = [
     [0, 0, 0],
     [1, 1, 1],
@@ -14,73 +28,75 @@ const winningCombos = [
 /*----- app's state (variables) -----*/
 let board; 
 
-// let randomNumber = Math.floor(Math.random() * 3);
-// console.log(randomNumber)
 /*----- cached element references -----*/
+const resultEl = document.querySelector('h2');
+const buttonEl = document.getElementById('btn')
+const buttonEl2 = document.getElementById('btn2')
 const slotOne = document.getElementById('slot1')
 const slotTwo = document.getElementById('slot2')
 const slotThree = document.getElementById('slot3')
-const resultEl = document.querySelector('h2');
-const buttonEl = document.querySelector('button')
-
+const imgOne = document.getElementById('one')
+const imgTwo = document.getElementById('two')
+const imgThree = document.getElementById('three')
 /*----- event listeners -----*/
 buttonEl.addEventListener('click', pullLever)
-
+buttonEl2.addEventListener('click', init)
 
 /*----- functions -----*/
+init()
 function init() {
-    board = [null, null, null];
-    // slotOne.addEventListener('click', pullLever)
-    // slotTwo.addEventListener('click', pullLever)
-    // slotThree.addEventListener('click', pullLever)
+    board = [null, null, null]
+    slotOne.replaceChildren(imgOne)
+    slotTwo.replaceChildren(imgTwo)
+    slotThree.replaceChildren(imgThree)
 }
 function pullLever() {
-    // board = [
-    //     Math.floor(Math.random() * 3),
-    //     Math.floor(Math.random() * 3),
-    //     Math.floor(Math.random() * 3)
-    // ];
-    renderSlotOne();
-    renderSlotTwo();
-    renderSlotThree();
+    board = [
+        Math.floor(Math.random() * 3),
+        Math.floor(Math.random() * 3),
+        Math.floor(Math.random() * 3)
+    ];
+    renderSlotOne()
+    renderSlotTwo()
+    renderSlotThree()
     checkWin()
-}
+} 
 function checkWin() {
     if (everyHelper(board, winningCombos[0])) {
            renderJackpotMessage()
        } else if (everyHelper(board, winningCombos[1])) {
            renderWinningMessage()
        } else if (everyHelper(board, winningCombos[2])) {
+           renderWinningMessage()
        } else {
            renderLosingMessage()
        }
 } 
 function renderSlotOne() {
     if (board[0] === 0) {
-        slotOne.replaceWith(seven)
+        imgOne.replaceWith(seven)
     } else if (board[0] === 1) {
-        slotOne.replaceWith(cherry)
+        imgOne.replaceWith(cherry)
     } else if (board[0] === 2) {
-        slotOne.replaceWith(heart)
-    }
+        imgOne.replaceWith(heart)
+    } 
 } 
-
 function renderSlotTwo() {
     if (board[1] === 0) {
-        slotTwo.replaceWith(seven)
+        imgTwo.replaceWith(seven2)
     } else if (board[1] === 1) {
-        slotTwo.replaceWith(cherry)
+        imgTwo.replaceWith(cherry2) 
     } else if (board[1] === 2) {
-        slotTwo.replaceWith(heart)
+        imgTwo.replaceWith(heart2)
     }
 }
 function renderSlotThree() {
     if (board[2] === 0) {
-        slotThree.replaceWith(seven)
+        imgThree.replaceWith(seven3)
     } else if (board[2] === 1) {
-        slotThree.replaceWith(cherry)
+        imgThree.replaceWith(cherry3)
     } else if (board[2] === 2) {
-        slotThree.replaceWith(heart)
+        imgThree.replaceWith(heart3)
     }
 }
 //Result Prompts
@@ -93,9 +109,6 @@ function renderLosingMessage() {
 function renderWinningMessage() {
     resultEl.innerText = "YOU WIN :) PLAY AGAIN?"
 }
-board = [0, 0, 0]
-console.log(board)
-// console.log(winningCombos[0])
 
 // helper functions
 function everyHelper(array, winCombo) {
